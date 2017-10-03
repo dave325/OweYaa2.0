1a174f9d0ee94047b92dd431cdcde13aca97322f
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-03 19:59:50 -0400

    Deply 2

--- vendor/laravel/lumen-framework/config/app.php
+++ vendor/laravel/lumen-framework/config/app.php
@@ -40,4 +40,18 @@ return [
     */
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
+     /*
+    |--------------------------------------------------------------------------
+    | Logging Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure the log settings for your application. Out of
+    | the box, Laravel uses the Monolog PHP logging library. This gives
+    | you a variety of powerful log handlers / formatters to utilize.
+    |
+    | Available Settings: "single", "daily", "syslog", "errorlog"
+    |
+    */
+
+    'log' => 'errorlog',
 ];

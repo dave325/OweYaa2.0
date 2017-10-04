cfed05b8ae058dfacf1c9a4b11e41ae2f34d5904
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-03 21:40:12 -0400

    Deploy 4

--- resources/views/index.php
+++ resources/views/index.php
@@ -10,41 +10,41 @@
         <script src="views/app.js"></script>
 
         <!-- Company Controllers -->
-        <script src="/views/frontend/controllers/company/browse-interns.controller.js"></script>
-        <script src="/views/frontend/controllers/company/dashboard.controller.js"></script>
-        <script src="/views/frontend/controllers/company/favorite-interns.controller.js"></script>
-        <script src="/views/frontend/controllers/company/project-submission.controller.js"></script>
+        <script src="views/frontend/controllers/company/browse-interns.controller.js"></script>
+        <script src="views/frontend/controllers/company/dashboard.controller.js"></script>
+        <script src="views/frontend/controllers/company/favorite-interns.controller.js"></script>
+        <script src="views/frontend/controllers/company/project-submission.controller.js"></script>
 
         <!-- Home Page controllers -->
-        <script src="/views/frontend/controllers/home/company.controller.js"></script>
-        <script src="/views/frontend/controllers/home/home.controller.js"></script>
-        <script src="/views/frontend/controllers/home/veteran.controller.js"></script>
-        <script src="/views/frontend/controllers/home/contact.controller.js"></script>
-        <script src="/views/frontend/controllers/home/register.controller.js"></script>
+        <script src="views/frontend/controllers/home/company.controller.js"></script>
+        <script src="views/frontend/controllers/home/home.controller.js"></script>
+        <script src="views/frontend/controllers/home/veteran.controller.js"></script>
+        <script src="views/frontend/controllers/home/contact.controller.js"></script>
+        <script src="views/frontend/controllers/home/register.controller.js"></script>
 
         <!-- Veteran Controllers -->
-        <script src="/views/frontend/controllers/veteran/career.controller.js"></script>
-        <script src="/views/frontend/controllers/veteran/internship.controller.js"></script>
-        <script src="/views/frontend/controllers/veteran/profile.controller.js"></script>
+        <script src="views/frontend/controllers/veteran/career.controller.js"></script>
+        <script src="views/frontend/controllers/veteran/internship.controller.js"></script>
+        <script src="views/frontend/controllers/veteran/profile.controller.js"></script>
 
         <!-- Directive Controllers -->
-        <script src="/views/frontend/directives/footer/footer.directive.js"></script>
-        <script src="/views/frontend/directives/navbar/navbar.directive.js"></script>
-        <script src="/views/frontend/directives/footer/footer.controller.js"></script>
-        <script src="/views/frontend/directives/navbar/navbar.controller.js"></script>
+        <script src="views/frontend/directives/footer/footer.directive.js"></script>
+        <script src="views/frontend/directives/navbar/navbar.directive.js"></script>
+        <script src="views/frontend/directives/footer/footer.controller.js"></script>
+        <script src="views/frontend/directives/navbar/navbar.controller.js"></script>
 
         <!-- Services/Factories -->
-        <script src="/views/frontend/services/authentication.service.js"></script>
-        <script src="/views/frontend/services/user.service.js"></script>
+        <script src="views/frontend/services/authentication.service.js"></script>
+        <script src="views/frontend/services/user.service.js"></script>
 
         <!-- Modal Controllers -->
-        <script src="/views/frontend/modals/home/login/login.modal.controller.js"></script>
-        <script src="/views/frontend/modals/veteran/action/action.modal.controller.js"></script>
-        <script src="/views/frontend/modals/veteran/career/career.modal.controller.js"></script>
-        <script src="/views/frontend/modals/veteran/education/education.modal.controller.js"></script>
-        <script src="/views/frontend/modals/veteran/journal/journal.modal.controller.js"></script>
-        <script src="/views/frontend/modals/veteran/skill/skill.modal.controller.js"></script>
-        <script src="/views/frontend/modals/veteran/social/social.modal.controller.js"></script>
+        <script src="views/frontend/modals/home/login/login.modal.controller.js"></script>
+        <script src="views/frontend/modals/veteran/action/action.modal.controller.js"></script>
+        <script src="views/frontend/modals/veteran/career/career.modal.controller.js"></script>
+        <script src="views/frontend/modals/veteran/education/education.modal.controller.js"></script>
+        <script src="views/frontend/modals/veteran/journal/journal.modal.controller.js"></script>
+        <script src="views/frontend/modals/veteran/skill/skill.modal.controller.js"></script>
+        <script src="views/frontend/modals/veteran/social/social.modal.controller.js"></script>
 
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -62,7 +62,7 @@
 
 		 <!--Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-	    <link href="/assets/stylesheets/style.css" rel="stylesheet"/>
+	    <link href="assets/stylesheets/style.css" rel="stylesheet"/>
 
     </head>
     <body ng-view>

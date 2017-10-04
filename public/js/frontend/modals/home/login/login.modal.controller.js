9337993e7a59951905f443837b232e717acf00b5
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-03 22:45:49 -0400

    login test

--- public/js/frontend/modals/home/login/login.modal.controller.js
+++ public/js/frontend/modals/home/login/login.modal.controller.js
@@ -33,6 +33,7 @@
         'type' : loginvm.type
       }
       Authentication.setToken(checkUser).then(function(data){
+        console.log(data);
         if(data){
           User.getCurrentUser(Authentication.getToken()).then(function(data){
             loginvm.close(data.data.user);

776b0b51fee20665826cee0923dcd1d0978502c1
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-04 21:26:49 -0400

    Add proxy trust

--- public/js/frontend/modals/home/login/login.modal.controller.js
+++ public/js/frontend/modals/home/login/login.modal.controller.js
@@ -33,7 +33,7 @@
         'type' : loginvm.type
       }
       Authentication.setToken(checkUser).then(function(data){
-        console.log(data.config.data);
+        console.log(data);
         if(data){
           User.getCurrentUser(Authentication.getToken()).then(function(data){
             loginvm.close(data.data.user);

11715c8316811bf6289607867484de10250f29c2
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-08 13:03:00 -0400

    Updated eloquent

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -6,6 +6,7 @@ use App\MilitaryUser;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Auth;
 class ExampleController extends Controller
 {
     private $apiCall;

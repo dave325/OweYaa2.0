7ebe2f3b07a3b78a4f18ea76f645456e3398fb05
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-08 17:23:59 -0400

    Updated eloquent

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -25,7 +25,7 @@ class ExampleController extends Controller
          $user->name = $credentials['username'];
                  try {
                      // attempt to verify the credentials and create a token for the user
-                     if (! $token = app('auth:veteran')->attempt($credentials)) {
+                     if (! $token = app('auth')->attempt($credentials)) {
                          return response()->json(['error' => 'invalid_credentials'], 401);
                      }
                  } catch (JWTException $e) {

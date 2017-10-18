78c7ae0dee7f3bd9a3f164f7a6f54c2e1b40319e
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-10-18 13:52:15 -0400

    Working on ui for education modal

--- app/MilitaryUser.php
+++ app/MilitaryUser.php
@@ -23,13 +23,14 @@ class MilitaryUser extends Model implements AuthenticatableContract, Authorizabl
      public $incrementing = false;
      // Remove default timestamp from eloquent
      public $timestamps = false;
-
+     // Array to hold edcation
+     private $education = array();
       /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-     protected $fillable = ['name','password','email','mos', 'location', 'phone', 'career1', 'career2', 'desired1', 'desired2', 'prev1', 'prev2', 'bio', 'language', 'education', 'mentor','social'];
+     protected $fillable = ['name','password','email','mos', 'location', 'phone', 'career1', 'career2', 'desired1', 'desired2', 'prev1', 'prev2', 'bio', 'language', 'mentor','social'];
     
      /**
      * The attributes excluded from the model's JSON form.

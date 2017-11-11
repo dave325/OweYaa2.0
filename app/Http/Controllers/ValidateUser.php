d46b41373d6bfb58eb152db5ecc64d454c26fae5
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-11-11 18:31:04 -0500

    Veteran UI

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -41,22 +41,12 @@ class ValidateUser extends Controller
             return response()->json(['token_absent'], $e->getStatusCode());
     
         }
-        $user->contactinfo = [
-            'name' => $userCheck->name,
-            'email' => $userCheck->email,
-            'mos' => $userCheck->mos,
-            'location' => $userCheck->location,
-            'phone' => $userCheck->phone,
-            'social' => Skill::with('militaryUser')->where('name','=',$userCheck->name)->get()
-        ];
-       
         $user->career1 = $userCheck->career1;
         $user->career2 = $userCheck->career2;
         $user->prev1 = $userCheck->prev1;
         $user->prev2 = $userCheck->prev2;
         $user->bio = $userCheck->bio;
-        //$user->course = MilitaryUser::with('course')->where('name','=',$userCheck->name)->get();
-        $user->skill = MilitaryUser::with('skill' , 'language', 'wantedSkills', 'availability', 'mentor', 'course')->where('name','=',$userCheck->name)->get();
+        $user->skill = MilitaryUser::with('skill' , 'language', 'wantedSkills', 'availability', 'mentor', 'course', 'social')->where('name','=',$userCheck->name)->get();
        
         // the token is valid and we have found the user via the sub claim
         return response()->json(compact('user'));

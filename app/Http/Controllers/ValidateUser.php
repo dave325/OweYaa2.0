b4a6852f8bc5c162c7c81b0cbf998526a9512560
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-11-11 15:07:07 -0500

    Veteran UI

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -51,7 +51,7 @@ class ValidateUser extends Controller
         $user->prev1 = $userCheck->prev1;
         $user->prev2 = $userCheck->prev2;
         $user->bio = $userCheck->bio;
-        $user->course = MilitaryUser::with('course.course')->where('name','=',$user->name)->get();
+        $user->course = Course::with('militaryUser')->where('name','=',$user->name)->get();
         $user->skill = $user::find($user->name)->skill;
         $user->language = $user::find($user->name)->language;
         $user->social = $user::find($user->name)->social;

b238d5837e14a5d3fd4b45df6e41becdac755372
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-11-14 18:42:07 -0500

    Veteran modal javascript fixes

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -49,7 +49,7 @@ class ValidateUser extends Controller
 
     public function updateEducation(Request $request){
         $credentials = $request->only('contact_info');
-        return response()->json($credentials);
+        return $credentials;
         $education = ContactInfo::where('name', '=', $credentials)-update($request);
     }
 }

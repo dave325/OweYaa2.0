b923d5aca38b2ad9df008f2d8a1a966e391133c5
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-11-14 20:59:56 -0500

    Veteran modal javascript fixes

--- public/js/frontend/modals/veteran/portfolio/portfolio.modal.controller.js
+++ public/js/frontend/modals/veteran/portfolio/portfolio.modal.controller.js
@@ -24,6 +24,7 @@
       }).then(function(data){
         User.setUser(data.data);
         console.log(User.getUser());
+        portfoliovm.close();
       },function(data){
         console.log(data);
       });

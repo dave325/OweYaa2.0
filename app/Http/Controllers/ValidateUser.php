598b228f07646e322370ca256c258a7b52aea582
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-11-20 14:05:35 -0500

    Veteran page UX

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -63,7 +63,7 @@ class ValidateUser extends Controller
             //return response()->json(compact('credentials'));
             Education::where('name', '=', $credentials['contact_info']['name'])->update($credentials['education']);
             foreach($credentials['bootcamp'] as $bootcamp){
-                TableModels\Bootcamp::where('name', '=', $credentials['contact_info']['name'])->where("bootcamp", "=" ,$bootcamp['bootcamp'])->update(['bootcamp' =>$bootcamp['bootcamp']]);
+                TableModels\Bootcamp::where('name', '=', $credentials['contact_info']['name'])->where("bootcamp", "=" ,$bootcamp['bootcamp'])->update(['bootcamp' =>$bootcamp['updatedCamp']]);
             }
             return response()->json(true);
         }else{

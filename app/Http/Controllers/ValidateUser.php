f7c954edadc60536aac94ba39598e9ae40e92eec
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-11-21 13:15:11 -0500

    Update UX for veteran page - beginning stages

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -89,6 +89,8 @@ class ValidateUser extends Controller
         $user->fill($request->all());
         if(app('auth')->guard($this->apiCall)->authenticate()){
             $credentials = $request->only('contact_info','prev_career_fields');
+            $credentials['prev_career_fields'][0]['name'] = 'david';
+            $credentials['prev_career_fields'][1]['name'] = 'david';
             $user->prevCareerFields()->saveMany([
                 new TableModels\PreviousCareerField($credentials['prev_career_fields'][0]),
                 new TableModels\PreviousCareerField($credentials['prev_career_fields'][1]),

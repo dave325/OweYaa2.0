cb6945cfc205675e83e592b6050d056f5f5e6305
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-12-30 21:00:55 -0500

    Updating Veteran UI

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -5,7 +5,6 @@
 (function(){
     $(document).ready(function(){
         $('.box').hover(function(){
-            $(this).children('infoBox').height($(this).outerHeight);
             $(this).children('infoBox').slideToggle();
         });
     });

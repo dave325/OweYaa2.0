64bd88d61a0111fb537e79c6177b34a1821da847
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-12-30 20:54:31 -0500

    Updating Veteran UI

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -2,19 +2,10 @@
  * Home Page 
  */
 
- var elem = document.getElementsByClassName('box');
- elem.addEventListener('mouseover',showInfo);
- function showInfo(){
-     var height = this.clientHieght;
-     var outerHeight = this.childNodes[0].clientHieght;
-     var pos = 0;
-     var id = setInterval(frame, 10);
-     function frame(){
-        if (height == outerHeight) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.height = pos + 'px';
-        }
-    }
-}
+(function(){
+    $(document).ready(function(){
+        $('.box').hover(function(){
+            $(this).slideToggle();
+        });
+    });
+})();

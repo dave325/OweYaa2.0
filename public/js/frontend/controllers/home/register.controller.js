5336d46777729f522c30380979f9099da5ef6f61
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2017-12-30 22:31:55 -0500

    Updating Veteran UI

--- public/js/frontend/controllers/home/register.controller.js
+++ public/js/frontend/controllers/home/register.controller.js
@@ -13,10 +13,10 @@
 		// Will make a call to the server and php file
 		vm.doRegister = function(credentials){
 			var user = {
-				name : vm.credentials.username,
-				password : vm.credentials.password,
-				email : vm.credentials.email,
-				type : vm.credentials.type
+				name : credentials.username,
+				password : credentials.password,
+				email : credentials.email,
+				type : credentials.type
 			};
 			console.log(user);
 			return;

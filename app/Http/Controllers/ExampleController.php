8c7f549eb7b9ed3fe007c6b8d717f494d2c2dd6b
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-03 21:57:46 -0500

    Updating Veteran UI

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -26,7 +26,7 @@ class ExampleController extends Controller
     }
     public function loginTest(Request $request){
          // grab credentials from the request
-         $credentials = $request->only('name', 'password');
+         $credentials = $request->only('username', 'password');
                  try {
                      // attempt to verify the credentials and create a token for the user
                      if (! $token = app('auth')->guard($this->apiCall)->attempt($credentials)) {

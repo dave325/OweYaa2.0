91c863a99cd69101990fc78c6c56c0c445ae690f
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-06 13:58:44 -0500

    Updating Register

--- public/js/angularApp.js
+++ public/js/angularApp.js
@@ -80,7 +80,7 @@
      */
     $rootScope.$on('$routeChangeStart', function (event) {
         if (!User.isLoggedIn() && $location.url().substring(1,9) == 'veteran/' || $location.url().substring(1,9) == 'company/') {
-          event.precentDefault();  
+          event.preventDefault();  
           //$location.url('/');
         }
     });

17458d0c31192f096c5787f174da7b01768b445d
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-07 14:02:26 -0500

    Updating Register

--- public/js/frontend/directives/navbar/navbar.controller.js
+++ public/js/frontend/directives/navbar/navbar.controller.js
@@ -36,14 +36,12 @@
        */
       modal.result
          .then(function (data) {
-           $timeout(function(){
             var curUser = data.contact_info.username;
             if(data.type == 1){
               $location.path('/company/' + curUser +'/dashboard');
-              }else{
-                $location.path('/veteran/' + curUser + '/profile');
-              }
-            },1500);
+            }else{
+              $location.path('/veteran/' + curUser + '/profile');
+            }
          },function (reason) {
            console.log(reason);
          })

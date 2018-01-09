8a8006dae63a6cbf09c25667ab7c1bccc81498be
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-09 14:27:21 -0500

    Integrating ng-file-upload for file handling to Lumen

--- config/filesystems.php
+++ config/filesystems.php
@@ -1,9 +1,16 @@
 <?php
 return [
+    "default" => "local",
     'disks' =>[
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/resources/profile_pics')
+            'root' => storage_path('app'),
+            'url' => env('APP_URL').'/resources/profile_pics'
         ],
     ],
+    'storage' => [
+        'driver' => 'local',
+        'root'   => storage_path(),
+    ],
+
 ];

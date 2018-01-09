703e3ac4fd612d0e2cbbc56e998e445e80d0e0bc
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-09 10:27:09 -0500

    Fixing File load issues

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -51,9 +51,9 @@ class ValidateUser extends Controller
     public function updateContact(Request $request){
         if(app('auth')->guard()->authenticate()){
             $credentials = $request->only('contact_info');
-            $pic = $request->file('pic');
+            $pic = $request->file('pic')->getRealPath();
             TableModels\ContactInfo::where('username', '=', $credentials['contact_info']['username'])->update($credentials['contact_info']);
-            Storage::disk('local')->putFileAs('/', $request->file('pic'), $credentials['contact_info']['username'] . '.doc');
+            Storage::disk('local')->putFileAs('/', $pic, $credentials['contact_info']['username'] . '.doc');
             $directories = Storage::disk('local');
             return response()->json($directories);
         }else{

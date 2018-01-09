0a6dcf3ac5d1dadb004dd4c98b991404814323d0
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-09 15:08:01 -0500

    Integrating ng-file-upload for file handling to Lumen

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -52,9 +52,10 @@ class ValidateUser extends Controller
     public function uploadFiles(Request $request){
         $credentials = $request->only('username');
         //$pic = $request->file('file')->storeAs('resources/profile_pics', $credentials['username'] . 'doc');
+        
         Storage::disk('local')->putFileAs('/resources/profile_pics', $request->file('file'), $credentials['username'] . 'doc');
         $directories = Storage::disk('local');
-        return response()->json($directories);
+        return response()->json($request->file('file'));
     }
     public function updateContact(Request $request){
         if(app('auth')->guard()->authenticate()){

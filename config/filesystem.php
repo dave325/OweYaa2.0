2a96ccb5bad40cabd75872973c167264ebb19433
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-09 09:23:19 -0500

    Contact Info Modal API Test

--- config/filesystem.php
+++ config/filesystem.php
@@ -1,9 +1,11 @@
 <?php
 return [
-    'public' => [
-        'driver' => 'local',
-        'root' => storage_path('app/resources/profile_pics'),
-        'url' => env('APP_URL').'/storage',
-        'visibility' => 'private',
+    'disks' =>[
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/resources/profile_pics'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'private',
+        ],
     ],
 ];

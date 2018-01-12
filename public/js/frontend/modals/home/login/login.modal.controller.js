ba1967459375ddd77e2a9542cd134e6c414c0d6c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-11 19:54:01 -0500

    Updating date objects to reflect proper changes in modal

--- public/js/frontend/modals/home/login/login.modal.controller.js
+++ public/js/frontend/modals/home/login/login.modal.controller.js
@@ -37,7 +37,7 @@
           User.getCurrentUser().then(function(data){
             for(var i = 0; i < 2; i++){
               if(data.data.user.prev_career_fields[i].start_date != null){
-                data.data.user.prev_career_fields[i].start_date = new Date(data.data.user.prev_career_fields[i].start_date);
+                data.data.user.prev_career_fields[i].start_date = new Date(data.data.user.prev_career_fields[i].start_date.substring(0,3),data.data.user.prev_career_fields[i].start_date.substring(5,6),data.data.user.prev_career_fields[i].start_date.substring(8,9) );
               }
               if(data.data.user.prev_career_fields[i].end_date != null){
                 data.data.user.prev_career_fields[i].end_date = new Date(data.data.user.prev_career_fields[i].end_date);

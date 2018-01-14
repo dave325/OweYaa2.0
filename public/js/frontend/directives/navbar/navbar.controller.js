1bc613d8db457c4b99784906364d34c975287ffb
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-13 22:56:32 -0500

    Updating models to allow user to sign in

--- public/js/frontend/directives/navbar/navbar.controller.js
+++ public/js/frontend/directives/navbar/navbar.controller.js
@@ -39,10 +39,11 @@
           navbarvm.message = "Thank you. Please wait until you are successfully logged in.";
           navbarvm.showMessage = true;
            $timeout(function(){
-            var curUser = data.contact_info.username;
             if(data.type == 1){
+              var curUser = data.username;
               $location.path('/company/' + curUser +'/dashboard');
             }else{
+              var curUser = data.contact_info.username;
               $location.path('/veteran/' + curUser + '/profile');
             }
           },3000);

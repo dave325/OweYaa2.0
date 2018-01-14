0b97000a7129a6b07fcf8d7511abe0f715a1e654
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-13 23:49:23 -0500

    Fixed error on check. User information now pulled properly

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -39,6 +39,7 @@ class ValidateUser extends Controller
             return response()->json(['token_absent'], $e->getStatusCode());
     
         }
+        return response()->json($request->input('type'));
         if($request->input('type') == 0){
             $user = User::with('contactInfo','skill' , 'language', 'wantedSkills', 'availability', 'certifications','mentor', 'course', 'social', 'education', 'careerSearch', 'goals','events', 'bootcamp', 'actionTask', 'prevCareerFields', 'careerGoals', 'hobbies', 'interviews')->where('username','=',$userCheck->username)->first();
         }

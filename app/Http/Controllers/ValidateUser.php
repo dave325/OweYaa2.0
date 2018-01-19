b02f667a7d1130577f78c2c8dcb0dc6783df0d9c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-19 18:21:24 -0500

    Implementing Stripe

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -47,6 +47,7 @@ class ValidateUser extends Controller
         }
         elseif($request->input('type') == 1){
             $user = User::with('company','companyFavorite','companyProject','CompanySearch')->where('username','=',$userCheck->username)->first();
+            $user['stripe_key'] = env('publishable_key');
         }else{
             return response()->json(['user_not_found'], 404);
         }

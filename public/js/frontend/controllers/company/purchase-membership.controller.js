ca0419a5364300f8f1ea3ed3b792df1030cece37
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-19 22:02:09 -0500

    Added angular-stripe file

--- public/js/frontend/controllers/company/purchase-membership.controller.js
+++ public/js/frontend/controllers/company/purchase-membership.controller.js
@@ -9,8 +9,27 @@
 
         // Create an instance of Elements
         var elements = stripe.elements();
+
+        // Custom styling can be passed to options when creating an Element.
+        // (Note that this demo uses a wider set of styles than the guide below.)
+        var style = {
+            base: {
+            color: '#32325d',
+            lineHeight: '18px',
+            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+            fontSmoothing: 'antialiased',
+            fontSize: '16px',
+            '::placeholder': {
+                color: '#aab7c4'
+            }
+            },
+            invalid: {
+            color: '#fa755a',
+            iconColor: '#fa755a'
+            }
+        };
         // Create an instance of the card Element
-        vm.payment= elements.create('card');
+        vm.payment= elements.create('card', {style: style});
 
         // Add an instance of the card Element into the `card-element` <div>
         vm.payment.mount('#card-elements');

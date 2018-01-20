7d42427545f604c8a5acebf50b8428d911e9df54
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-19 21:45:49 -0500

    Added angular-stripe file

--- public/js/frontend/controllers/company/purchase-membership.controller.js
+++ public/js/frontend/controllers/company/purchase-membership.controller.js
@@ -6,25 +6,12 @@
         vm.user = User.getUser();
         vm.payment = {};
         vm.charge = function charge() {
-            return stripe.card.createToken(vm.payment)
-              .then(function (response) {
-                console.log('token created for card ending in ', response.card.last4)
-                var payment = angular.copy(vm.member.payment)
-                payment.card = undefined
-                payment.token = response.id
-                return $http.post('/api/payment/test', payment)
-              })
+            $http.post('/api/payment/test', payment)
               .then(function (payment) {
-                console.log('successfully submitted payment for $', payment.amount)
-              })
-              .catch(function (err) {
-                if (err.type && /^Stripe/.test(err.type)) {
-                  console.log('Stripe error: ', err.message)
-                }
-                else {
-                  console.log('Other error occurred, possibly with your API', err.message)
-                }
-              })
+                console.log(payment)
+              },function(data){
+                console.log(data);
+              });
           }
     }
     angular.module('oweyaa')

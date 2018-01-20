58258321ba7f7a8f246835616f2abed6eae6d649
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-19 21:58:15 -0500

    Added angular-stripe file

--- public/js/frontend/controllers/company/purchase-membership.controller.js
+++ public/js/frontend/controllers/company/purchase-membership.controller.js
@@ -4,7 +4,27 @@
     function purchaseMembershipCtrl(User,$http) {
         var vm = this;
         vm.user = User.getUser();
-        vm.payment = {};
+        // Create a Stripe client
+        var stripe = Stripe(vm.user.stripe_key);
+
+        // Create an instance of Elements
+        var elements = stripe.elements();
+        // Create an instance of the card Element
+        vm.payment= elements.create('card', {style: style});
+
+        // Add an instance of the card Element into the `card-element` <div>
+        vm.payment.mount('#card-element');
+
+        // Handle real-time validation errors from the card Element.
+        card.addEventListener('change', function(event) {
+        var displayError = document.getElementById('card-errors');
+        if (event.error) {
+            displayError.textContent = event.error.message;
+        } else {
+            displayError.textContent = '';
+        }
+        });
+
         vm.charge = function charge() {
             $http.post('/api/payment/test', vm.payment)
               .then(function (payment) {

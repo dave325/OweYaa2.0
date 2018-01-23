d3694c4f25013eeb10e1e1a697430eb2a94e49ad
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-23 09:42:37 -0500

    UI for purchase memebership and created payment modal

--- public/js/frontend/modals/company/purchaseMembership/purchaseMembership.modal.controller.js
+++ public/js/frontend/modals/company/purchaseMembership/purchaseMembership.modal.controller.js
@@ -68,7 +68,7 @@
      // Add an instance of the card Element into the `card-element` <div>
      purchaseMembershipModalvm.payment.card.mount('#card-number');
      purchaseMembershipModalvm.payment.cardCvc.mount('#card-cvc');
-     purchaseMembershipModalvm.payment.cardCvc.mount('#card-expiry');
+     purchaseMembershipModalvm.payment.cardExpiry.mount('#card-expiry');
      // Check the availability of the Payment Request API first.
     paymentRequest.canMakePayment().then(function(result) {
         if (result) {

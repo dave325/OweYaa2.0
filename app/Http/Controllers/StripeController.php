fa6e410137022e9424a0d66c4493a2f15aee1669
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-23 17:44:03 -0500

    UI for purchase memebership and created payment modal

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -17,7 +17,7 @@ class StripeController extends Controller{
             // Use Stripe's library to make requests...
             if(Stripe\Customer::retrieve($info['user']['company']['stripetoken'])){
                 $user["customer"] = Stripe\Customer::retrieve($info['user']['company']['stripetoken']);
-                $user["customer"]->source = $info['tempToken'];
+                $user["customer"]['source'] = $info['tempToken'];
                 $user["customer"]->save();
                 $user["charge"] = Stripe\Charge::create(array(
                     "amount" => $info['type']['total']['amount'],

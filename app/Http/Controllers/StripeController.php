76c1db5cdc6621de96c1f5f0b3b2c9774d1a8df6
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-23 17:28:46 -0500

    UI for purchase memebership and created payment modal

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -49,18 +49,39 @@ class StripeController extends Controller{
             return response()->json($err);
           } catch (\Stripe\Error\RateLimit $e) {
             // Too many requests made to the API too quickly
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          
+            return response()->json($err);
           } catch (\Stripe\Error\InvalidRequest $e) {
             // Invalid parameters were supplied to Stripe's API
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          
+            return response()->json($err);
           } catch (\Stripe\Error\Authentication $e) {
             // Authentication with Stripe's API failed
             // (maybe you changed API keys recently)
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          
+            return response()->json($err);
           } catch (\Stripe\Error\ApiConnection $e) {
             // Network communication with Stripe failed
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          
+            return response()->json($err);
           } catch (\Stripe\Error\Base $e) {
             // Display a very generic error to the user, and maybe send
             // yourself an email
+            $body = $e->getJsonBody();
+            $err  = $body['error'];
+          
+            return response()->json($err);
           } catch (Exception $e) {
             // Something else happened, completely unrelated to Stripe
+            return response()->json($e);
           }
     }
 }

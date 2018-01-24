bd7af2985722b87a1ab0050a131c10f89888dd5d
Deepak K <drk3931@gmail.com>
2018-01-24 13:00:05 -0500

    Refactored into style.css company dashboard nad projects dashboard settings and added a new controller for project dashboard

---
+++ public/js/frontend/controllers/company/project-dashboard.controller.js
@@ -0,0 +1,14 @@
+(function(){
+    //Injector will protect against minification
+    favoriteInternsCtrl.$inject = [];
+    function projectDashboardCtrl() {
+        var vm = this;
+        //Retrieve project information
+        
+
+        
+    }
+      angular.module('oweyaa')
+        .controller('projectDashboardCtrl', projectDashboardCtrl);
+    
+    })();

e6a6fceb89fa4f858db3f91f2302ff6f8ebe1054
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-01-26 09:24:59 -0500

    Commmented code

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -43,8 +43,8 @@ class ValidateUser extends Controller
             return response()->json(['user_not_found'], 404);
         }
         if($request->input('type') == 0){
-            $user = User::find($userCheck->username);
-            $user::with('milUser.skill');
+            
+            $user = User::with('milUser.skill');
             //$user = User::with('contactInfo','skill' , 'language', 'wantedSkills', 'availability', 'certifications','mentor', 'course', 'social', 'education', 'careerSearch', 'goals','events', 'bootcamp', 'actionTask', 'prevCareerFields', 'careerGoals', 'hobbies', 'interviews')->where('username','=',$userCheck->username)->first();
         }
         elseif($request->input('type') == 1){

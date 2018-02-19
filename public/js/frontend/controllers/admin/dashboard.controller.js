c56b07e846a653feb47c3bbefd7b54ee1a375cf3
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 15:15:49 -0500

    Fixed admin login

--- public/js/frontend/controllers/admin/dashboard.controller.js
+++ public/js/frontend/controllers/admin/dashboard.controller.js
@@ -24,7 +24,7 @@
         }
 
         adminDash.retrieveVet = function(){
-            AdminService.retrieveVet(adminDash.internSearch.username).then(function(response){
+            AdminService.retrieveVet(adminDash.internSearch).then(function(response){
                 console.log(response);
             },function(error){
                 console.log(error);

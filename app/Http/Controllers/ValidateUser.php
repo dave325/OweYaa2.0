dc09a6094d2500818f68d8ead324ca694185947f
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 10:06:19 -0500

    Fixed admin login

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -39,7 +39,7 @@ class ValidateUser extends Controller
             return response()->json(['token_absent'], $e->getStatusCode());
     
         }
-        if(!$request->filled('type') || $request->input('type') != $userCheck->type){
+        if(!$request->has('type') || $request->input('type') != $userCheck->type){
             return response()->json(['user_not_found1'], 404);
         }
         elseif($request->has('type') && $request->input('type') == 0){

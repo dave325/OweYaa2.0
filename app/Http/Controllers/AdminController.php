20672fa019e81af70b2eab13195fd941f3779b6d
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 09:45:01 -0500

    Fixed admin login

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,7 +18,7 @@ class AdminController extends Controller{
             // something went wrong whilst attempting to encode the token
             return response()->json(['error' => 'could_not_create_token'], 500);
         }
-        $test = \ValidateUser::checks();
+        $test = ValidateUser::checks();
         return response()->json(compact('test'));
         // all good so return the token
         return response()->json(['token' => $token],200);

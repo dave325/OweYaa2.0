59249a4646ef26dd0dbc0b9d2bd78e552786aa9f
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 15:56:36 -0500

    Fixed admin login

--- public/js/frontend/controllers/admin/dashboard.controller.js
+++ public/js/frontend/controllers/admin/dashboard.controller.js
@@ -4,12 +4,25 @@
         var adminDash = this;
         AdminService.retrieveCompInfo().then(function(response){
             adminDash.compInfo = response.data.user.companies;
-            console.log(adminDash.compInfo);
+            for(let comp in adminDash.compInfo){
+                if(comp.company_project === undefined){
+                    adminDash.numProjects;
+                    adminDash.vetInProject;
+                }else{
+                    adminDash.numProjects += comp.company_project.length;
+                    adminDash.vetInProject += comp.company_project.intern.length;
+                }
+            }
         },function(error){
             console.log(error);
         })
         AdminService.retrieveAllVet().then(function(response){
             adminDash.vet = response.data.users;
+            for(let vet in adminDash.vet){
+                if(vet.interview != undefined && vet.interview.length > 0){
+                    adminDash.numInterviewed++;
+                }
+            }
         },function(error){
             console.log(error);
         });
@@ -17,20 +30,6 @@
         adminDash.numProjects = 0;
         adminDash.numInterviewed = 0;
         adminDash.vetInProject = 0;
-        for(let comp in adminDash.compInfo){
-            if(comp.company_project === undefined){
-                adminDash.numProjects;
-                adminDash.vetInProject;
-            }else{
-                adminDash.numProjects += comp.company_project.length;
-                adminDash.vetInProject += comp.company_project.intern.length;
-            }
-        }
-        for(let vet in adminDash.vet){
-            if(vet.interview != undefined && vet.interview.length > 0){
-                adminDash.numInterviewed++;
-            }
-        }
 
         adminDash.retrieveVet = function(){
             AdminService.retrieveVet(adminDash.internSearch).then(function(response){

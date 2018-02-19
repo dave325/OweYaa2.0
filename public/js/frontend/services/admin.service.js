41b258f9e14e95e46e426bdaaafb5add850ba30f
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 15:21:16 -0500

    Fixed admin login

--- public/js/frontend/services/admin.service.js
+++ public/js/frontend/services/admin.service.js
@@ -9,13 +9,23 @@
                 return $http.post('/api/admin/updateUser', data);
             },
             retrieveAllVet:function(){
-                return $http.post('/api/admin/retrieveAllVet');
+                $http.post('/api/admin/retrieveAllVet').then(function(response){
+                    console.log(response);
+                    return response.data.user;    
+                },function(error){
+                    console.log(error);
+                });
             },
             retrieveVet:function(username){
                  return $http.post('/api/admin/retrieveVet',username);
             },
             retrieveCompInfo:function(){
-                return $http.post('/api/admin/retrieveComp');
+                return $http.post('/api/admin/retrieveComp').then(function(response){
+                    console.log(response);
+                    return response.data.user;    
+                },function(error){
+                    console.log(error);
+                });
             }
         }
         return adminService;

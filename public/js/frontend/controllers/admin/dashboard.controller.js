7a9ce93c6b705ec0e5e4df8afedd13a0226c55a0
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 10:14:05 -0500

    Fixed admin login

--- public/js/frontend/controllers/admin/dashboard.controller.js
+++ public/js/frontend/controllers/admin/dashboard.controller.js
@@ -9,12 +9,17 @@
         adminDash.numInterviewed = 0;
         adminDash.vetInProject = 0;
         for(let comp in adminDash.compInfo){
+            if(comp.company_project === undefined){
+                adminDash.numProjects = 0;
+                adminDash.vetInProject = 0;
+            }
             adminDash.numProjects += comp.company_project.length;
             adminDash.vetInProject += comp.company_project.intern.length;
         }
         for(let vet in adminDash.vet){
-            if(vet.interview.length > 0)
-            adminDash.numInterviewed++;
+            if(vet.interview.length > 0){
+                adminDash.numInterviewed++;
+            }
         }
 
         adminDash.retrieveVet = function(){

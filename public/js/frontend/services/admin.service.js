236e35273c5c19c521f27de2f2f10ee3f1973ba1
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 19:50:02 -0500

    Fixed admin login

--- public/js/frontend/services/admin.service.js
+++ public/js/frontend/services/admin.service.js
@@ -1,38 +1,53 @@
-(function(){
-    adminSer.$inject = ['$http','$window'];
-    function adminSer($http,$window){
+(function () {
+    adminSer.$inject = ['$http', '$window'];
+    function adminSer($http, $window) {
         const adminService = {
-            login:function(user){
-                return $http.post('/api/admin/login',user).then(
-                    function(data){
-                      if(data.data.token){
-                        $window.sessionStorage.setItem('token', data.data.token);
-                        // Returns the user data object to the login modal
+            login: function (user) {
+                return $http.post('/api/admin/login', user).then(
+                    function (data) {
+                        if (data.data.token) {
+                            $window.sessionStorage.setItem('token', data.data.token);
+                            // Returns the user data object to the login modal
+                            return data;
+                        } else {
+                            return data;
+                        }
+                    }, function (data) {
+                        // Returns information from error call
                         return data;
-                      }else{
-                        return data;
-                      }
-                    },function(data){
-                      // Returns information from error call
-                      return data;
                     }
-                  )
+                )
             },
-            updateUser:function(data){
+            getUser: function (user) {
+                if (Authentication.getToken() == null) {
+                    return;
+                } else {
+                    // returns the http call that register the user variable in the database
+                    return $http({
+                        url: '/api/check',
+                        method: 'POST',
+                        data: { user },
+                        headers: {
+                            "Authorization": "Bearer " + Authentication.getToken()
+                        }
+                    });
+                }
+            },
+            updateUser: function (data) {
                 return $http.post('/api/admin/updateUser', data);
             },
-            retrieveAllVet:function(){
+            retrieveAllVet: function () {
                 return $http.post('/api/admin/retrieveAllVet');
             },
-            retrieveVet:function(username){
-                 return $http.post('/api/admin/retrieveVet',username);
+            retrieveVet: function (username) {
+                return $http.post('/api/admin/retrieveVet', username);
             },
-            retrieveCompInfo:function(){
+            retrieveCompInfo: function () {
                 return $http.post('/api/admin/retrieveComp');
             }
         }
         return adminService;
     }
     angular.module('oweyaa')
-        .factory('AdminService',adminSer);
+        .factory('AdminService', adminSer);
 })();

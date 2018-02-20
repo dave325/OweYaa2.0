2c279b274c4316bb01aef348e8c49c4fda72238c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-19 19:14:15 -0500

    Fixed admin login

--- public/js/frontend/controllers/admin/login.controller.js
+++ public/js/frontend/controllers/admin/login.controller.js
@@ -1,22 +1,25 @@
-(function(){
-    adminLoginCtrl.$inject = ['AdminService','User', '$location'];
-    function adminLoginCtrl(AdminService,User,$location){
+(function () {
+    adminLoginCtrl.$inject = ['AdminService', 'User', '$location'];
+    function adminLoginCtrl(AdminService, User, $location) {
         var adminLogin = this;
         adminLogin.user = {};
 
-        adminLogin.loginUser = function(){
-            AdminService.login(adminLogin.user).then(function(response){
-                if(response.status === 200){
-                    console.log(response);
-                    User.setUser(response.data.user);
-                    $location.path('/admin/dashboard');
+        adminLogin.loginUser = function () {
+            AdminService.login(adminLogin.user).then(function (response) {
+                if (response.status === 200) {
+                    User.getCurrentUser(response.data.token).then(function (data) {
+                        User.setUser(data.data.user);
+                        $location.path('/admin/dashboard');
+                    }, function (data) {
+                        console.log(data);
+                    });
                 }
-            },function(error){
+            }, function (error) {
                 console.log(error);
             });
         }
     }
 
     angular.module('oweyaa')
-        .controller("adminLoginCtrl",adminLoginCtrl);
+        .controller("adminLoginCtrl", adminLoginCtrl);
 })();

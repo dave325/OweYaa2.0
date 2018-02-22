a868a9233762681fd25ed9f9f18928b25078e510
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-02-21 22:18:53 -0500

    Updating dahsboard to show information for veteran

--- public/js/frontend/controllers/admin/dashboard.controller.js
+++ public/js/frontend/controllers/admin/dashboard.controller.js
@@ -31,7 +31,7 @@
         });
 
         adminDash.retrieveVet = function(){
-            if(adminDash.internSearch.username){
+            if(adminDash.internSearch.username || adminDash.internSearch.username.length > 0){
                 AdminService.retrieveVet(adminDash.internSearch).then(function(response){
                     console.log(response.data.user);
                 },function(error){

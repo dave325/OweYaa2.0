82f55483fa4a57935007d6457a353fe3a887bf0f
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-03-14 19:58:11 -0400

    Fix issue with logout not updating when on home screen

--- public/js/frontend/directives/navbar/navbar.controller.js
+++ public/js/frontend/directives/navbar/navbar.controller.js
@@ -256,7 +256,7 @@
     };
 
     navbarvm.isLogout = function(link){
-      console.log(link);
+      console.log(link.click != 'logout' && link.name != 'Company Dashboard' || link.name != 'Veteran Profile');
       return link.click != 'logout' && link.name != 'Company Dashboard' || link.name != 'Veteran Profile';
     }
 

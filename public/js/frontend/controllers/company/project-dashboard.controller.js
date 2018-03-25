a26b0a2e7271dc3635451cdf81385f6c501726a6
Deepak K <drkgla@Deepaks-MacBook-Pro-2.local>
2018-03-25 02:09:53 -0400

    Revamped project dashboard

--- public/js/frontend/controllers/company/project-dashboard.controller.js
+++ public/js/frontend/controllers/company/project-dashboard.controller.js
@@ -10,6 +10,8 @@
         vm.projects = User.getUser().company_project;
         //ng-repeat on projects.
 
+        
+
         function intern(name,email,hours)
         {
             return {
@@ -34,6 +36,9 @@
             new milestone("Submit Patents","11/17/2018","todo"),
             new milestone("Create Presentation Models","09/16/2018","todo"),
         ];
+
+        vm.projectDescription = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        vm.projectTitle="Project Title";
 
         vm.interns = [
             new intern("Intern One","intern@gmail.com",24),
@@ -75,7 +80,7 @@
 
         }
 
-        vm.editDescription = function () {
+        vm.editDescription = function (title,description) {
             $uibModal.open({
                 templateUrl: getModalPath('project-dashboard-Description'),
                 controller: function ($scope, $uibModalInstance) {
@@ -92,11 +97,11 @@
 
         }
 
-        vm.editMilestones = function () {
+        vm.editMilestones = function (milestones) {
 
 
             $uibModal.open({
-                templateUrl: getModalPath('project-dashboard-Milestones'),
+                templateUrl: getModalPath('project-dashboard-mstones'),
 
                 controller: function ($scope, $uibModalInstance) {
 
@@ -109,13 +114,25 @@
                     $scope.cancel = function () {
                         $uibModalInstance.dismiss('cancel');
                     };
+
+                    $scope.onDelete = function(milestone)
+                    {
+
+                        vm.milestones.splice(milestones.indexOf(milestone),1);
+                    }
+
+                    $scope.onAdd = function(milestoneDescription,date,status)
+                    {
+                        
+                        vm.milestones.push(new milestone(milestoneDescription,date,status));
+                    }
                 },
                 windowClass:winClass
             });
 
         }
 
-
+      
     }
     angular.module('oweyaa')
         .controller('projectDashboardCtrl', projectDashboardCtrl);

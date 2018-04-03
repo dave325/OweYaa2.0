6d03bb4d480737d8650cbd8809fe05403f25aa37
Rick <RikkuG491@gmail.com>
2018-04-03 11:20:11 -0400

    Updated filter

--- public/js/frontend/filters/sortInterns.filter.js
+++ public/js/frontend/filters/sortInterns.filter.js
@@ -1,7 +1,61 @@
+// Check if input is null, then return everybody.
+// Otherwise, return people matching that skill.
+
+// Needs to include "people who have no skills".
+
 (function(){
-    sortInterns.$inject = [];
-    function sortInterns(){
-        
+
+    // Ensures that the parameters match specifically for sortInterns.
+    // sortInterns is the filter name
+    // Injector will protector against minification.
+    sortInterns.$inject = ['input', 'interns'];
+
+    /**
+     * Function name: sortInterns
+     * Description: Sorts interns based on skills. Checks if the input (search
+     * query) is null, if it is null, then return all of the interns. Otherwise,
+     * return interns matching that skill.
+     *
+     * @param input - The input search query.
+     * @param interns - An array containing all interns, and their information.
+    **/
+
+    function sortInterns(input, interns){
+
+      // If the input search query is empty, or null...
+      if(input == null){
+
+        // Return everybody.
+        return interns;
+
+      }
+
+      // Otherwise, the input search query is not empty.
+
+      // Create a new array to store interns that match the input search query.
+      var filteredInterns = [];
+
+      // Iterate through the interns array, searching for interns that match the
+      // input search query. Index is the current index number, starting at 0.
+      // Interns.length is the total number of interns.
+      for(var index = 0; index < interns.length; index++){
+
+        // The current intern index in the array.
+        var intern = interns[index];
+
+        // If the value of the input search query matches that of the intern's
+        // skill, add the intern to the filteredInterns array.
+        // intern.skill is the intern's skill. This method checks only for one
+        // skill per intern, for now.
+        if(input.trim().toUpperCase() == intern.skill){
+          filteredInterns.push(intern);
+        }
+
+      }
+
+      // Return filtered array.
+      return filteredInterns;
+
     }
     angular.module('oweyaa')
         .filter('sortInterns', sortInterns);

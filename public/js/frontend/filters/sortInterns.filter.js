db7270aec7339a7529a45d2b9c08f5e48ed06a43
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-03 11:44:43 -0400

    Filter test

--- public/js/frontend/filters/sortInterns.filter.js
+++ public/js/frontend/filters/sortInterns.filter.js
@@ -21,7 +21,7 @@
     **/
 
     function sortInterns(interns, input){
-
+      console.log("reached");
       // If the input search query is empty, or null...
       if(input == null){
 
@@ -34,7 +34,7 @@
 
       // Create a new array to store interns that match the input search query.
       var filteredInterns = [];
-
+      console.log("reached2");
       // Iterate through the interns array, searching for interns that match the
       // input search query. Index is the current index number, starting at 0.
       // Interns.length is the total number of interns.

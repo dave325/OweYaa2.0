9262ef49e1ba3e8480682d4c89df47213251e790
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-10 08:43:54 -0400

    Fixed issue with copmany login, Caused by human error

--- public/js/frontend/controllers/company/project-submission.controller.js
+++ public/js/frontend/controllers/company/project-submission.controller.js
@@ -48,8 +48,9 @@ function projectSubmissionCtrl(User) {
       },function(error){
         console.log(error);
       });
+      */
     }
-    */
+  
   }
    angular.module('oweyaa')
 	.controller('projectSubmissionCtrl', projectSubmissionCtrl);

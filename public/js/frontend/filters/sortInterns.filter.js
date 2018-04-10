71436fdd1ec6abd5fc528e041320dd45021eca34
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-10 15:57:35 -0400

    Fixed browse intern page

--- public/js/frontend/filters/sortInterns.filter.js
+++ public/js/frontend/filters/sortInterns.filter.js
@@ -3,28 +3,29 @@
 
 // Needs to include "people who have no skills".
 
-(function(){
+(function () {
 
-    // Ensures that the parameters match specifically for sortInterns.
-    // sortInterns is the filter name
-    // Injector will protector against minification.
-    /**
-     * Function name: sortInterns
-     * Description: Sorts interns based on skills. Checks if the input (search
-     * query) is null, if it is null, then return all of the interns. Otherwise,
-     * return interns matching that skill.
-     *
-     * @param input - The input search query.
-     * @param interns - An array containing all interns, and their information.
-    **/
+  // Ensures that the parameters match specifically for sortInterns.
+  // sortInterns is the filter name
+  // Injector will protector against minification.
+  /**
+   * Function name: sortInterns
+   * Description: Sorts interns based on skills. Checks if the input (search
+   * query) is null, if it is null, then return all of the interns. Otherwise,
+   * return interns matching that skill.
+   *
+   * @param input - The input search query.
+   * @param interns - An array containing all interns, and their information.
+  **/
 
-    function sortInterns(input, interns){
+  function sortInterns() {
+    return function works(input, interns) {
       console.log("reached");
       // If the input search query is empty, or null...
-      if(interns == null){
+      if (interns == null) {
 
         // Return everybody.
-        return {interns:input};
+        return { interns: input };
 
       }
 
@@ -36,7 +37,7 @@
       // Iterate through the interns array, searching for interns that match the
       // input search query. Index is the current index number, starting at 0.
       // Interns.length is the total number of interns.
-      for(var index = 0; index < input.length; index++){
+      for (var index = 0; index < input.length; index++) {
 
         // The current intern index in the array.
         var intern = input[index];
@@ -45,16 +46,17 @@
         // skill, add the intern to the filteredInterns array.
         // intern.skill is the intern's skill. This method checks only for one
         // skill per intern, for now.
-        if(input.trim().toUpperCase() == input.skill){
+        if (input.trim().toUpperCase() == input.skill) {
           filteredInterns.push(input);
         }
 
       }
       console.log("reached3");
-      
-      // Return filtered array.
-      return {filteredInterns:filteredInterns};
 
+      // Return filtered array.
+      return { filteredInterns: filteredInterns };
     }
-    angular.module('oweyaa').filter('sortInterns', sortInterns);
+  }
+  angular.module('oweyaa')
+    .filter('sortInterns', sortInterns);
 })();

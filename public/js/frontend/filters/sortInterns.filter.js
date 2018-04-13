653e4ca01b9e2d7fbb6a757983b747b66ff00ac3
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-13 11:47:53 -0400

    Fixing the filter

--- public/js/frontend/filters/sortInterns.filter.js
+++ public/js/frontend/filters/sortInterns.filter.js
@@ -22,7 +22,7 @@
     return function works(interns, input) {
       console.log("reached");
       // If the input search query is empty, or null...
-      if (interns == null) {
+      if (input == null) {
 
         // Return everybody.
         return { interns: interns };
@@ -33,7 +33,6 @@
 
       // Create a new array to store interns that match the input search query.
       var filteredInterns = [];
-      console.log("reached1");
       // Iterate through the interns array, searching for interns that match the
       // input search query. Index is the current index number, starting at 0.
       // Interns.length is the total number of interns.

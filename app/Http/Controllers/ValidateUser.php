c7a9f6fbd92d72c9b3ce3a6cb5b51bdf53c51363
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-22 15:22:37 -0400

    Adding fav user functionality

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -1002,7 +1002,8 @@ class ValidateUser extends Controller
             // Retrieve Company projects where ismatched != true.
             $projects = TableModels\CompanyModels\CompanyFavorite::where('username','=',$userInfo['company_info']['username'])->get();
             foreach($projects as $proj){
-                $temp = User::with('contactInfo','skill','education','availability','monthAvailability','companyFavorite')->where('username', '=', $proj['internid'])->get();
+                $temp = User::with('contactInfo','skill','education','availability','monthAvailability')->where('username', '=', $proj['internid'])->get()->toArray();
+                $temp['favid'] = $proj['favid'];
                 array_push($internInfo,$temp);
             }
             // If successful, return a success response.

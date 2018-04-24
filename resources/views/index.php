fd06003de7871ccc0c00d42ff8b073326f78a1d5
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-24 08:01:56 -0400

    Updateing mobile views

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html ng-app="oweyaa">
     <head>
+        <meta name="viewport" content="width=device-width" />
         <base href="/" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.6.5/angular-route.min.js"></script>

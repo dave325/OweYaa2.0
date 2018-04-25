49fa2181b753d33bf6aa19fb76fa20cf0ec79b85
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-24 20:01:53 -0400

    Update Education Vet UI

--- public/js/frontend/controllers/company/project-submission.controller.js
+++ public/js/frontend/controllers/company/project-submission.controller.js
@@ -50,7 +50,7 @@ function projectSubmissionCtrl(User) {
       console.log(vm.submissionForm.skills.split(','));
       for(let i = 0;i < vm.submissionForm.skills.length;i++){
         let temp = vm.submissionForm.skills[i];
-        vm.submissionForm.company_skillls[i].push({
+        vm.submissionForm.company_skillls.push({
           skillid: vm.submissionForm.username + i,
           projid: vm.submissionForm.projid,
           skill:temp

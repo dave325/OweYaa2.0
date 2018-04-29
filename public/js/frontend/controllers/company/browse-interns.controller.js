f4c6489e6daa355d06ace01c2cc2cb8985b164e0
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-29 12:28:26 -0400

    make it better

--- public/js/frontend/controllers/company/browse-interns.controller.js
+++ public/js/frontend/controllers/company/browse-interns.controller.js
@@ -31,6 +31,7 @@
             method: 'POST'
         }).then(function (response) {
             vm.users = response.data.user;
+            console.log(vm.users);
             User.getFavUsers(vm.user).then(function (response) {
                 for (let j = 0; j < vm.users.length; j++) {
                     for (let i = 0; i < response.data.projects.length; i++) {

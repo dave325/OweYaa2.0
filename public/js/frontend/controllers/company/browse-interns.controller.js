eaafb21ce409bbca4a56b935a48c85ea7d812537
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-04-29 13:53:51 -0400

    make it better

--- public/js/frontend/controllers/company/browse-interns.controller.js
+++ public/js/frontend/controllers/company/browse-interns.controller.js
@@ -32,7 +32,7 @@
         }).then(function (response) {
             vm.users = response.data.user;
             User.getFavUsers(vm.user).then(function (response) {
-                console.log(removeEventListener.users);
+                console.log(vm.users);
                 for (let j = 0; j < vm.users.length - 1; j++) {
                     for (let i = 0; i < response.data.projects.length -1; i++) {
                         if(vm.users[j].contact_info != null && vm.users[j].contact_info.username === response.data.projects[i].user.contact_info.username){

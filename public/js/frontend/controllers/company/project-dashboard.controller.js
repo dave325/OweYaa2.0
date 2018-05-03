f17f066e4c0ea04a52b4a846f6627962184bd4f8
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-03 15:22:31 -0400

    Fixing names for copmany side

--- public/js/frontend/controllers/company/project-dashboard.controller.js
+++ public/js/frontend/controllers/company/project-dashboard.controller.js
@@ -86,6 +86,18 @@
                 controller: function ($scope, $uibModalInstance) {
 
                     $scope.curProj = vm.curProj;
+                    vm.careerOptions = ['developer', 'designer', 'marketing', 'sales', 'customer service'];
+                    vm.jobtype = ['job', 'internship', 'project'];
+                    vm.istemp = [
+                        {
+                            name: 'Temporary',
+                            value: 0
+                        },
+                        {
+                            name: 'Permanent',
+                            value: 0
+                        }
+                    ];
                     $scope.ok = function () {
                         vm.postNewDescription(vm.projectTitle, vm.projectDescription);
                         $uibModalInstance.close();

058f6701fd5422e56f72aecac96947f5e6d91629
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-04 13:10:07 -0400

    Updating JWT

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -72,7 +72,7 @@ class ExampleController extends Controller
         $user = new User();
         try {
             $currUser = AuthController::me()->getData(true, 5);
-            return response()->json($currUser);
+            return response()->json($currUser['original']);
             // If the type of user specified doesn't exist, or if the user's type
             // doesn't match the type that the database listed for this user,
             // the response states that the user is not found. This is a 404 error.

475fa7ff8d5d63ef02f5bbf60173c335dff9f2b8
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-04 13:27:23 -0400

    Updating JWT

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -76,30 +76,30 @@ class ExampleController extends Controller
             // doesn't match the type that the database listed for this user,
             // the response states that the user is not found. This is a 404 error.
             if (!array_key_exists('type', $request) || intval($request['type']) != $currUser['type']) {
-                return response()->json(['user_not_found2'], 404);
+                return response()->json(['user_not_found2']);
             }
 
             // If the type of user specified exists and is equal to 0, the user is
             // a Veteran user, and the attributes are filled in for this Veteran user.
-            elseif (!array_key_exists('type', $request)  && intval($request['type']) == 0) {
+            elseif (!array_key_exists('type', $request)  && intval($request['type']) === 0) {
                 //$user= User::with(['milUser.skill','milUser.contactInfo'])->where('username','=',$userCheck->username)->get();
                 $user = User::with('contactInfo', 'skill', 'language', 'wantedSkills', 'availability', 'monthAvailability', 'certifications', 'mentor', 'course', 'social', 'education', 'careerSearch', 'goals', 'events', 'bootcamp', 'actionTask', 'prevCareerFields', 'careerGoals', 'hobbies', 'interviews')->where('username', '=',$currUser['username'])->first();
-                $user['project'] = TableModels\CompanyModels\CompanyProject::where('internid', '=', $currUser-['username'])->first();
-                return response()->json(['user' => $user], 200);
+                $user['project'] = TableModels\CompanyModels\CompanyProject::where('internid', '=', $currUser['username'])->first();
+                return response()->json(['user' => $user]);
             }
 
             // If the type of user specified exists and is equal to 1, the user is
             // a company, and the attributes are filled in for this company user.
             elseif (!array_key_exists('type', $request) && intval($request['type']) == 1) {
                 $user = User::with('companyInfo', 'companyFavorite', 'companyProject', 'CompanySearch', 'membershipToken')->where('username', '=', $currUser['username'])->first();
-                return response()->json(['user' => $user], 200);
+                return response()->json(['user' => $user]);
             }
 
             // If the type of user specified exists, is equal to 2, and the user has
             // administrator access, the user is an administrator.
             elseif (!array_key_exists('type', $request) && intval($request['type']) == 2) {
                 $user = User::with('contactInfo')->first();
-                return response()->json(['user' => $user], 200);
+                return response()->json(['user' => $user]);
             }
             // Otherwise, the user doesn't exist. A user not found response will be
             // sent.

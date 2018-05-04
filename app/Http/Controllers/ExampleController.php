031a7270fd40491c28f5c888ec39a0e1a324624d
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-04 10:53:02 -0400

    Updating JWT

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -27,7 +27,7 @@ class ExampleController extends Controller
          $credentials = $request->only('username', 'password');
         try {
             // attempt to verify the credentials and create a token for the user
-            if (! $token = auth('auth')->attempt($credentials)) {
+            if (! $token = $app->auth()->guard('api')->attempt($credentials)) {
                 return response()->json(['error' => 'invalid_credentials'], 401);
             }
         } catch (JWTException $e) {

8259b0df37693939036192e59d2935af43f79667
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-04 10:35:53 -0400

    Updating JWT

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,7 +25,7 @@ class AuthController extends Controller
      */
     public function login(Request $request)
     {
-        $credentials = $request->only(['username', 'password']);
+        $credentials = $request->only('username', 'password');
 
         try {
             if (!$token = auth()->attempt($credentials)) {

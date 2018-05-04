d2f8e3b3ca51cf50b5389400d16272908e2483d9
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-04 11:35:19 -0400

    Updating JWT

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -71,43 +71,43 @@ class ExampleController extends Controller
         // Create a variable to store data about the current user.
         $user = new User();
         try {
-            $currUser = JWTAuth::userOrFail();
+            $currUser = JWTAuth::user();
 
-        // If the type of user specified doesn't exist, or if the user's type
-        // doesn't match the type that the database listed for this user,
-        // the response states that the user is not found. This is a 404 error.
-        if (!$request->has('type') || $request->input('type') != $currUser->type) {
-            return response()->json(['user_not_found'], 404);
-        }
+            // If the type of user specified doesn't exist, or if the user's type
+            // doesn't match the type that the database listed for this user,
+            // the response states that the user is not found. This is a 404 error.
+            if (!$request->has('type') || $request->input('type') != $currUser->type) {
+                return response()->json(['user_not_found'], 404);
+            }
 
-        // If the type of user specified exists and is equal to 0, the user is
-        // a Veteran user, and the attributes are filled in for this Veteran user.
-        elseif ($request->has('type') && $request->input('type') == 0) {
-            //$user= User::with(['milUser.skill','milUser.contactInfo'])->where('username','=',$userCheck->username)->get();
-            $user = User::with('contactInfo', 'skill', 'language', 'wantedSkills', 'availability', 'monthAvailability', 'certifications', 'mentor', 'course', 'social', 'education', 'careerSearch', 'goals', 'events', 'bootcamp', 'actionTask', 'prevCareerFields', 'careerGoals', 'hobbies', 'interviews')->where('username', '=', $userCheck->username)->first();
-            $user['project'] = TableModels\CompanyModels\CompanyProject::where('internid', '=', $currUser->username)->first();
-            return response()->json(['user' => $user], 200);
-        }
+            // If the type of user specified exists and is equal to 0, the user is
+            // a Veteran user, and the attributes are filled in for this Veteran user.
+            elseif ($request->has('type') && $request->input('type') == 0) {
+                //$user= User::with(['milUser.skill','milUser.contactInfo'])->where('username','=',$userCheck->username)->get();
+                $user = User::with('contactInfo', 'skill', 'language', 'wantedSkills', 'availability', 'monthAvailability', 'certifications', 'mentor', 'course', 'social', 'education', 'careerSearch', 'goals', 'events', 'bootcamp', 'actionTask', 'prevCareerFields', 'careerGoals', 'hobbies', 'interviews')->where('username', '=', $userCheck->username)->first();
+                $user['project'] = TableModels\CompanyModels\CompanyProject::where('internid', '=', $currUser->username)->first();
+                return response()->json(['user' => $user], 200);
+            }
 
-        // If the type of user specified exists and is equal to 1, the user is
-        // a company, and the attributes are filled in for this company user.
-        elseif ($request->has('type') && $request->input('type') == 1) {
-            $user = User::with('companyInfo', 'companyFavorite', 'companyProject', 'CompanySearch', 'membershipToken')->where('username', '=', $currUser->username)->first();
-            return response()->json(['user' => $user], 200);
-        }
+            // If the type of user specified exists and is equal to 1, the user is
+            // a company, and the attributes are filled in for this company user.
+            elseif ($request->has('type') && $request->input('type') == 1) {
+                $user = User::with('companyInfo', 'companyFavorite', 'companyProject', 'CompanySearch', 'membershipToken')->where('username', '=', $currUser->username)->first();
+                return response()->json(['user' => $user], 200);
+            }
 
-        // If the type of user specified exists, is equal to 2, and the user has
-        // administrator access, the user is an administrator.
-        elseif ($request->has('type') && $request->input('type') == 2 && $request->has('admin')) {
-            $user = User::with('contactInfo')->first();
-            return response()->json(['user' => $user], 200);
-        }
+            // If the type of user specified exists, is equal to 2, and the user has
+            // administrator access, the user is an administrator.
+            elseif ($request->has('type') && $request->input('type') == 2 && $request->has('admin')) {
+                $user = User::with('contactInfo')->first();
+                return response()->json(['user' => $user], 200);
+            }
 
-        // Otherwise, the user doesn't exist. A user not found response will be
-        // sent.
-        else {
-            return response()->json(['user_not_found'], 404);
-        }
+            // Otherwise, the user doesn't exist. A user not found response will be
+            // sent.
+            else {
+                return response()->json(['user_not_found'], 404);
+            }
         } catch (\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
             // do something
         }

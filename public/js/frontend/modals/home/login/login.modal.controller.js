6995ee69dca947d4190a9a4933952adfed26b048
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-05 00:13:32 -0400

    Updating JWT

--- public/js/frontend/modals/home/login/login.modal.controller.js
+++ public/js/frontend/modals/home/login/login.modal.controller.js
@@ -35,7 +35,7 @@
       Authentication.login(checkUser).then(function(data){
         if(data.status === 200){
           User.getCurrentUser(checkUser).then(function(data){
-            User.setUser(data.data.user);
+            User.setUser(data);
             loginvm.close(data);
           },function(data){
             loginvm.formError = "Username or password does not exist.<br/> Please try again.";

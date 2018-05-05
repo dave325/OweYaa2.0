6912f1c69cc452a868b42016389c05100c63c334
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-05 16:35:51 -0400

    Updating to new authentication

--- public/js/frontend/modals/home/login/login.modal.controller.js
+++ public/js/frontend/modals/home/login/login.modal.controller.js
@@ -35,9 +35,8 @@
       Authentication.login(checkUser).then(function(data){
         if(data.status === 200){
           User.getCurrentUser(checkUser).then(function(data){
-            console.log(data);
-            User.setUser(data);
-            loginvm.close(data);
+            User.setUser(data.user);
+            loginvm.close(data.user);
           },function(data){
             loginvm.formError = "Username or password does not exist.<br/> Please try again.";
           });

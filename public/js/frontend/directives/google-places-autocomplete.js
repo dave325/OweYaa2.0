6c9c319a33bb998e70ccc4f56c8e424731011d60
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-12 13:13:09 -0400

    Updating login modal style

--- public/js/frontend/directives/google-places-autocomplete.js
+++ public/js/frontend/directives/google-places-autocomplete.js
@@ -85,7 +85,7 @@ angular.module("ngAutocomplete", [])
                 }
                 google.maps.event.addListener(scope.gPlace, 'place_changed', function () {
                     var result = scope.gPlace.getPlace();
-                    console.log(result.location);
+                    console.log(result);
                     if (result !== undefined) {
                         if (result.address_components !== undefined) {
 

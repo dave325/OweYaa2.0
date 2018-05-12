5c7186d3e9bacc341d9c6e35458b35e716629b23
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-11 21:39:02 -0400

    Updating login modal style

--- public/js/frontend/directives/google-places-autocomplete.js
+++ public/js/frontend/directives/google-places-autocomplete.js
@@ -1,20 +1,32 @@
-angular.module('oweyaa').directive('googleplace', function() {
+angular.module('oweyaa').directive('googleplace', ['$state', function ($state) {
     return {
         require: 'ngModel',
-        link: function(scope, element, attrs, model) {
+        link: function (scope, element, attrs, model) {
             var options = {
                 types: [],
-                componentRestrictions: {
-                    country: 'us'
-                }
-            };
+                componentRestrictions: {}
+                };
             scope.gPlace = new google.maps.places.Autocomplete(element[0], options);
+            google.maps.event.addListener(scope.gPlace, 'place_changed', function (e) {
+                scope.$apply(function () {
+                    model.$setViewValue(element.val());
+                    var countrycode = "na";
+                    var latitude = 0;
+                    var longitude = 0;
+                    scope.secondplacebox = scope.chosenPlace;
+                    for (var i = 0; i < scope.gPlace.getPlace().address_components.length; i++) {
+                        if (scope.gPlace.getPlace().address_components[i].types[0] == 'country') {
+                            countrycode = scope.gPlace.getPlace().address_components[i].short_name;
+                        }
+                    }
+                    latitude = scope.gPlace.getPlace().geometry.location.lat().toFixed(6);
+                    longitude = scope.gPlace.getPlace().geometry.location.lng().toFixed(6);
+                    //$state.go('home');
+                    scope.Fn_setCookieData(latitude, longitude, countrycode);
 
-            google.maps.event.addListener(scope.gPlace, 'place_changed', function() {
-                scope.$apply(function() {
-                    model.$setViewValue(element.val());                
                 });
             });
         }
     };
-});
+}
+]);

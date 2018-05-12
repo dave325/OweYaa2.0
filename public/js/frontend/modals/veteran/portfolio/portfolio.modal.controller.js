34b98fbfe8099f39916e8d26aa6070dc029d8894
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-12 13:33:06 -0400

    Updating login modal style

--- public/js/frontend/modals/veteran/portfolio/portfolio.modal.controller.js
+++ public/js/frontend/modals/veteran/portfolio/portfolio.modal.controller.js
@@ -58,7 +58,7 @@
     }
     // Will make a call to the server and php file
     portfoliovm.doportfolio = function (modal, data) {
-      console.log($scope);
+      console.log($scope.autoCompleteDetails);
       return;
       portfoliovm.isDisabled = true;
       //Update server information

afde90664f41070c032c24f8778e855516ab0892
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-05-11 23:21:34 -0400

    Updating login modal style

--- public/js/frontend/directives/google-places-autocomplete.js
+++ public/js/frontend/directives/google-places-autocomplete.js
@@ -1,4 +1,4 @@
-angular.module('oweyaa').directive('googleplace', [, function () {
+angular.module('oweyaa').directive('googleplace', [ function () {
     return {
         require: 'ngModel',
         link: function (scope, element, attrs, model) {

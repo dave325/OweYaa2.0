0df9e1e130546992f6cc8a8ba8ed372a93d2f10d
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-06-02 22:54:45 -0400

    Adding functionality to select intern for project

--- public/js/frontend/controllers/company/browse-interns.controller.js
+++ public/js/frontend/controllers/company/browse-interns.controller.js
@@ -114,9 +114,9 @@
             User.addFavUser(favIntern).then(function (response) {
                 vm.users[user].isFav = true;
                 vm.resultInfo = "Successfully added candidate to favorites!";
-                /*$timeout(function () {
+                $timeout(function () {
                     vm.resultInfo = "";
-                }, 1500);*/
+                }, 1500);
                 console.log(response);
             }, function (error) {
                 console.log(error);

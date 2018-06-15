3f5300d3e533e0a562453031bed1ca89ca587e6c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-06-15 13:24:13 -0400

    Fixing company login

--- public/js/frontend/controllers/veteran/currentInternships.controller.js
+++ public/js/frontend/controllers/veteran/currentInternships.controller.js
@@ -1,16 +1,39 @@
-(function(){
+(function () {
 	//Injector will protect against minification
 	currentInternshipCtrl.$inject = ['$scope', '$uibModal', 'User'];
-	function currentInternshipCtrl($scope,$uibModal, User) {
+	function currentInternshipCtrl($scope, $uibModal, User) {
 		let vetIntern = this;
-		User.getAllProjects().then(function(response){
+		User.getAllProjects().then(function (response) {
 			console.log(response.data);
 			vetIntern.projects = response.data;
-		},function(error){
+			vm.totalItems = vetIntern.projects.length;
+			vm.currentPage = 1;
+			vm.itemsPerPage = 5;
+			$scope.$watch("vetIntern.currentPage", function () {
+				setPagingData(vm.currentPage);
+			});
+
+			function setPagingData(page) {
+				var pagedData = vetIntern.projects.slice(
+					(page - 1) * vm.itemsPerPage,
+					page * vm.itemsPerPage
+				);
+				vm.displayUsers = pagedData;
+			}
+			setPagingData(vm.currentPage);
+		}, function (error) {
 			console.log(error);
 		});
 	}
+
+	vetIntern.inProj = function (user) {
+		if (user.jobInfo.initiated == 1) {
+			user.inProj = true;
+		} else {
+			user.inProj = false;
+		}
+	}
 	angular
-	.module('oweyaa')
-	.controller('currentInternshipCtrl', currentInternshipCtrl);
+		.module('oweyaa')
+		.controller('currentInternshipCtrl', currentInternshipCtrl);
 })();

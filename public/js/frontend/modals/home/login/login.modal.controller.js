852372c18f710bd1f016a3578f58aadd8cbe7774
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-06-15 12:35:23 -0400

    Fixing company login

--- public/js/frontend/modals/home/login/login.modal.controller.js
+++ public/js/frontend/modals/home/login/login.modal.controller.js
@@ -37,7 +37,7 @@
         loginvm.isDisabled = true;
         if (data.status === 200) {
           User.getCurrentUser(checkUser).then(function (data) {
-            if (data.data.user.company_info.initiated === 0) {
+            if (data.data.user.type == 0 || data.data.user.company_info.initiated && data.data.user.company_info.initiated === 0 ) {
               User.setUser(data.data.user);
               loginvm.close(data.data.user);
             }else{

f18c7812aaf4ee7eb767db4e45290c534f9c48f8
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-06-17 16:21:32 -0400

    Mail integration

--- public/js/frontend/controllers/company/project-dashboard.controller.js
+++ public/js/frontend/controllers/company/project-dashboard.controller.js
@@ -1,7 +1,7 @@
 (function () {
     //Injector will protect against minification
-    projectDashboardCtrl.$inject = ['$scope', 'User', '$uibModal', '$http'];
-    function projectDashboardCtrl($scope, User, $uibModal, $http) {
+    projectDashboardCtrl.$inject = ['$scope', 'User', '$uibModal', '$http','$filter'];
+    function projectDashboardCtrl($scope, User, $uibModal, $http, $filter) {
 
         var vm = this;
 

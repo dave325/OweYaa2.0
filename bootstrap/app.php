35f62ed94b49ada9eb422e0116dd5a9c37e6d040
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-06-17 01:27:31 -0400

    Mail integration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -84,6 +84,7 @@ $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->configure('mail');
 
 /*
 |--------------------------------------------------------------------------

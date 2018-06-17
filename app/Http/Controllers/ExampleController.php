d354c65d0a32a511422f5a7d64f696462f0ea14c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-06-17 00:02:40 -0400

    Fixing admin side

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -85,6 +85,7 @@ class ExampleController extends Controller
         $user = new User();
         $request = $req->all();
         $currUser = AuthController::currUser();
+        return response()->json($currUser);
         // If the type of user specified doesn't exist, or if the user's type
         // doesn't match the type that the database listed for this user,
         // the response states that the user is not found. This is a 404 error.

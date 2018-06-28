5efc7ac73091aa03c0f46967be1cf6aecd5d03fc
Brian Kenji Aguilar <anjacular@gmail.com>
2018-06-28 15:38:04 -0400

    ModelsUpdate: Changed the Company Schemea and working on sub-schemeas

---
+++ models/companyuser.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose');
+
+var CompanySchema = new mongoose.Schema({
+    
+    email:String,
+    initiated: Date,
+
+    company_info: {
+        diversity:String,
+        number_of_employees:Number,
+        description:String,
+        departments: [String],
+        revenue:Number,
+        website:String
+    },
+
+    contact_info:{
+        first_name:String,
+        last_name:String,
+        phone_number:String,
+        address:String,
+        lat:Number,
+        lng:Number
+    },
+
+    company_project:{
+        //In-Progress
+        job_info:{
+            title:String,
+            description:String
+        },
+        candidates:[String],
+        //In-Progress
+        manager_info:{
+            first_name:String,
+            last_name:String,
+            email:String,
+            phone_number:String
+        },
+        milestones:[String]
+    }
+
+});
+
+module.exports = mongoose.model('Company',CompanySchema);

5ed8654e07696af27270bc10826d15f1f7e9895c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-11 19:07:42 -0400

    Register page fix

--- public/js/frontend/controllers/company/purchase-membership.controller.js
+++ public/js/frontend/controllers/company/purchase-membership.controller.js
@@ -1,34 +1,33 @@
-(function(){
+(function () {
     //Injector will protect against minification
-    purchaseMembershipCtrl.$inject = ['User','$http','$uibModal'];
-    function purchaseMembershipCtrl(User,$http,$uibModal) {
+    purchaseMembershipCtrl.$inject = ['User', '$http', '$uibModal'];
+    function purchaseMembershipCtrl(User, $http, $uibModal) {
         var vm = this;
         vm.user = User.getUser();
         // Opens modal to begin payment
-        vm.openPayment = function(payType){
-            if(User.getUser()){
+        vm.openPayment = function (payType) {
+            if (User.getUser()) {
                 var m = $uibModal.open({
                     templateUrl: '/js/frontend/modals/company/purchaseMembership/purchaseMembership.modal.view.html',
                     controller: 'purchaseMembershipModalCtrl',
                     controllerAs: 'purchaseMembershipModalvm',
-                    windowClass:"col-xs-12 col-md-4 col-md-offset-4 compModal",
-                    windowTopClass:"col-xs-12 ",
-                    resolve:{
-                        PayType:{type:payType}
+                    windowClass: "col-xs-12 col-md-4 col-md-offset-4 compModal",
+                    windowTopClass: "col-xs-12 ",
+                    resolve: {
+                        PayType: { type: payType }
                     }
                 });
 
                 m.result
                     .then(function (data) {
                         console.log(data);
-                    },function (reason) {
+                    }, function (reason) {
                         console.log(reason);
                     });
             }
         }
     }
     angular.module('oweyaa')
-    .controller('purchaseMembershipCtrl', purchaseMembershipCtrl);
-    
+        .controller('purchaseMembershipCtrl', purchaseMembershipCtrl);
+
 })();
-    

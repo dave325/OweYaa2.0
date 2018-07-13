ab33d8cc8f725312f90bf515dac6952ca0de696c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-12 23:16:13 -0400

    Fix register

--- public/js/frontend/controllers/company/project-dashboard.controller.js
+++ public/js/frontend/controllers/company/project-dashboard.controller.js
@@ -56,7 +56,7 @@
                     vm.curProj = vm.user.company_project[indexOfCurrentProject];
                     var myEl = angular.element( document.getElementsByClassName( 'projectLink' ) );
                     var active = angular.element( document.getElementsByClassName( 'active' ) );
-                    if(active != undefined){
+                    if(active != undefined && active.classList != undefined){
                         active.classList.remove('active');
                     }
                     myEl[indexOfCurrentProject].classList.add('actice');

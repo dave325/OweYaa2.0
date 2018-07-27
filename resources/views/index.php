2d5f8dec34851b2d66c97b6a1793d7d6cafaaf5d
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-27 12:51:07 -0400

    adding file handling function

--- resources/views/index.php
+++ resources/views/index.php
@@ -50,7 +50,7 @@
         <script src="js/frontend/directives/footer/footer.directive.js"></script>
         <script src="js/frontend/directives/navbar/navbar.directive.js"></script>
         <script src="js/frontend/directives/footer/footer.controller.js"></script>
-        <script src="js/frontend/directives/navbar/navbar.controller.js"></script>
+        <script src="js/frontend/directives/file-upload.controller.js"></script>
 
         <!-- Services/Factories -->
         <script src="js/frontend/services/authentication.service.js"></script>

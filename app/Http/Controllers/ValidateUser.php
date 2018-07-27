bad423a062e23039ee47377e71ecf5ab35c8def9
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-27 13:09:09 -0400

    adding file handling function

--- app/Http/Controllers/ValidateUser.php
+++ app/Http/Controllers/ValidateUser.php
@@ -916,6 +916,7 @@ class ValidateUser extends Controller
     public function updateFile(Request $request)
     {
         $info = $request->all();
+        return response()->json($info, 200);
         if ($isValid = $this->isValid()) {
             if (isset($info['delete']) && $info['delete']) {
 

67d5704b4d5f839c6e2791ca8672c732a7d4df78
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-29 00:21:30 -0400

    adding file handling function

--- public/js/frontend/controllers/company/browse-interns.controller.js
+++ public/js/frontend/controllers/company/browse-interns.controller.js
@@ -1,7 +1,7 @@
 (function () {
     //Injector will protect against minification
-    browseInternsCtrl.$inject = ['$scope', "User", "$http", "Authentication", "$uibModal", "$timeout",'location'];
-    function browseInternsCtrl($scope, User, $http, Authentication, $uibModal, $timeout,location) {
+    browseInternsCtrl.$inject = ['$scope', "User", "$http", "Authentication", "$uibModal", "$timeout",'$location'];
+    function browseInternsCtrl($scope, User, $http, Authentication, $uibModal, $timeout,$location) {
         var vm = this;
         vm.test = '';
         vm.resultInfo = "";
@@ -200,7 +200,7 @@
             }
         }
         vm.viewUser = function(user){
-            location.path('/vateran/').search({username: user.contact_info.username});
+            $location.path('/vateran/').search({username: user.contact_info.username});
         }
     }
     angular.module('oweyaa')

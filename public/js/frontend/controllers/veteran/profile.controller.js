bce24490b8586c094827ba7f7b25a64bda8c82e4
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-29 00:55:19 -0400

    adding file handling function

--- public/js/frontend/controllers/veteran/profile.controller.js
+++ public/js/frontend/controllers/veteran/profile.controller.js
@@ -7,7 +7,7 @@
 			User.returnUser($routeParams.veteranid).then(function (res) {
 				console.log(res.data.user);
 				$scope.user = res.data.user;
-				$scope.user.type = 0;
+				$scope.user.type = 1;
 				$scope.progress = calcProgress($scope.user);
 			}, function (err) {
 				location.path('/company/' + User.getUser().company_info.username + '/browase-interns')

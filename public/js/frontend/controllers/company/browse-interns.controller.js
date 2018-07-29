5d15c5584a5a2ee975770014ee99999d1f1f9a9c
David Dataram <daviddataram@Davids-MacBook-Pro.local>
2018-07-29 00:24:55 -0400

    adding file handling function

--- public/js/frontend/controllers/company/browse-interns.controller.js
+++ public/js/frontend/controllers/company/browse-interns.controller.js
@@ -200,7 +200,7 @@
             }
         }
         vm.viewUser = function(user){
-            $location.path('/vateran/'+ user.contact_info.username + '/profile').search({username: user.contact_info.username});
+            $location.path('/veteran/'+ user.contact_info.username + '/profile').search({username: user.contact_info.username});
         }
     }
     angular.module('oweyaa')
